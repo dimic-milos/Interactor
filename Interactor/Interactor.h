2fe548e638edea83ea4051ca245f46e731f812c8
Milos Dimic <dimic.milos@icloud.com>
2019-12-06 14:47:54 +0100

    Initial Commit

---
+++ Interactor/Interactor.h
@@ -0,0 +1,19 @@
+//
+//  Interactor.h
+//  Interactor
+//
+//  Created by Dimic Milos on 12/6/19.
+//  Copyright © 2019 Dimic Milos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Interactor.
+FOUNDATION_EXPORT double InteractorVersionNumber;
+
+//! Project version string for Interactor.
+FOUNDATION_EXPORT const unsigned char InteractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Interactor/PublicHeader.h>
+
+
